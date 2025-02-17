5e0b4011b3ccdbe6dab6205fe0e60492be6dc4af
root <thomas.rousseau19@gmail.com>
2025-02-17 09:24:31 +0000

    rr

---
+++ hello_world/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+short int n = 11;
+printf("L'entier n vaut %6hd \n", n);
+short int nn = 10;
+char test[5] = "lets";
+printf ("hello %s \n ", test);
+return (n);
+}
