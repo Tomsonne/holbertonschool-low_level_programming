759c08cb0c67bade65e3058f18d8cac9ee07c23b
root <thomas.rousseau19@gmail.com>
2025-02-19 10:20:38 +0000

    r

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,11 @@ int i;
 char c;
 
 for (i = 0 ; i < 10 ; i++)
-{
-for (c = 'a' ; c <= 'z' ; c++)
-{
-_putchar (c);
-}
-_putchar('\n');
-}
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar (c);
+		}
+	_putchar('\n');
+	}
 }
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include"main.h"
+/**
+ * _islower - Entry point
+ *@c: caractere a verifier
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	return (islower(c));
+}
