becc6497befa2e49372bb97e6d0782efb8316afb
root <thomas.rousseau19@gmail.com>
2025-02-19 13:02:02 +0000

    tet

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include"main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ *@c: caractere a verifier
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int c)
+{
+return (abs(c));
+}
