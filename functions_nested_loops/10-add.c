003c9cc43116cc5d2e3f218f6db424ad66aa7718
root <thomas.rousseau19@gmail.com>
2025-02-20 20:51:05 +0000

    r

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ *add - Entry point
+ *@x : caractere a verifier
+ *@y : azeazea
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+int c;
+
+c = x + y;
+return (c);
+}
