96072a2e0fe8d277b0c44b8a4d602d8ca0d6f815
root <thomas.rousseau19@gmail.com>
2025-02-21 13:32:47 +0000

    r

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,24 +9,24 @@
 
 int main(void)
 {
-unsigned long long int num1, num2, i, sum;
+double num1, num2, i, sum;
 
 sum = 0;
 num1 = 1;
 num2 = 2;
 i = 0;
 
-printf("%llu, ", num1);
-printf("%llu, ", num2);
+printf("%.0f, ", num1);
+printf("%.0f, ", num2);
 for (i = 0; i <= 95; i++)
 {
 sum = num1 + num2;
 num1 = num2;
 num2 = sum;
 if (i == 95)
-printf("%llu", sum);
+printf("%.0f", sum);
 else
-printf("%llu, ", sum);
+printf("%.0f, ", sum);
 }
 putchar('\n');
 return (0);
