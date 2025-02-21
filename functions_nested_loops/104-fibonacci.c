569c8846a77898f1dd872e2baf953f0787cc617f
root <thomas.rousseau19@gmail.com>
2025-02-21 12:14:28 +0000

    r

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *: caractere a verifier
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+unsigned long int num1, num2, i, sum;
+
+sum = 0;
+num1 = 1;
+num2 = 2;
+i = 0;
+
+printf("%lu, ", num1);
+printf("%lu, ", num2);
+for (i = 0; i <= 98; i++)
+{
+sum = num1 + num2;
+num1 = num2;
+num2 = sum;
+if (i == 98)
+printf("%lu", sum);
+else
+printf("%lu, ", sum);
+}
+putchar('\n');
+return (0);
+}
