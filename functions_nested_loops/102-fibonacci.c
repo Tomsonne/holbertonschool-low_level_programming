9b09aada46d70f83f51b9dcc8e02015ca3bb922e
root <thomas.rousseau19@gmail.com>
2025-02-21 10:19:42 +0000

    fibo

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *: caractere a verifier
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+long unsigned int num1, num2, i, sum;
+
+sum = 0;
+num1 = 1;
+num2 = 2;
+i = 0;
+
+for (i = 0; i <= 50; i++)
+{
+sum = num1 + num2;
+num1 = num2;
+num2 = sum;
+printf("%lu, ", sum);
+}
+putchar('\n');
+return (0);
+}
