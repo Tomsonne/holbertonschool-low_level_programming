e4d7cd0eed9daaf3df5874f0b8fbb6d56664178f
root <thomas.rousseau19@gmail.com>
2025-02-21 10:22:18 +0000

    fibo

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,15 @@
 
 int main(void)
 {
-long unsigned int num1, num2, i, sum;
+unsigned long int num1, num2, i, sum;
 
 sum = 0;
 num1 = 1;
 num2 = 2;
 i = 0;
 
+printf("%lu, ", num1);
+printf("%lu, ", num2);
 for (i = 0; i <= 50; i++)
 {
 sum = num1 + num2;
