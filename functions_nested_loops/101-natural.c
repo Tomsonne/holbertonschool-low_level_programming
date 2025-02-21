3dd1c280521183d0e1ed9fdb6f9628a8ecd67454
root <thomas.rousseau19@gmail.com>
2025-02-21 09:25:43 +0000

    bonus

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *: caractere a verifier
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 unsigned long int add3, add5, sum;
-int i ;
+int i;
 
 add3 = 0;
 add5 = 0;
@@ -16,12 +22,12 @@ for (i = 0; i < 1024; i++)
 	{
 		add3 = add3 + i;
 	}
-	else if ((i % 5) ==0)
+	else if ((i % 5) == 0)
 	{
 		add5 = add5 + i;
 	}
 }
 sum = add3 + add5;
 printf("%lu\n", sum);
-return(0);
+return (0);
 }
