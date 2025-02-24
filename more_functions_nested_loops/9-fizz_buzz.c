8333a003a66c892ce4c6bf55664b0826a2c99da1
root <thomas.rousseau19@gmail.com>
2025-02-24 14:02:14 +0000

    r

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ *main - Entry point
+ * : caractere a verifier
+ * fonction: ecris nombre
+ * Return: void
+ */
+
+
+int main(void)
+{
+int val, cent;
+
+cent = 100;
+for (val = 1; val <= cent ; val++)
+{
+
+if ((val % 3) == 0 && (val % 5) == 0)
+printf("FizzBuzz ");
+else if (val % 3 == 0)
+printf("Fizz ");
+else if ((val % 5) == 0)
+printf("Buzz ");
+else
+printf("%d ", val);
+
+}
+putchar('\n');
+
+return (0);
+}
