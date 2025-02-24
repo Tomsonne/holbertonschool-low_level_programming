c1e2a38bbd313219b0dd4c2e357be15f911f76a8
root <thomas.rousseau19@gmail.com>
2025-02-24 09:04:09 +0000

    a * b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ *mul - Entry point
+ *@x : caractere a verifier
+ *@y : azeazea
+ *int c: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
