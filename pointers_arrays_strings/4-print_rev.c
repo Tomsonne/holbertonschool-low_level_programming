23a0f49f2b4651f4761a186bc1aeeddb58d47481
root <thomas.rousseau19@gmail.com>
2025-02-25 12:45:20 +0000

    r

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_rev - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+int len;
+int i = 0;
+
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+for (i = 0; i < len+1 ; i++)
+_putchar(s[len-i]);
+_putchar('\n');
+}
