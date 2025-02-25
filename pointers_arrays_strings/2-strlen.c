a782d5e540e0ec8bf6e894d9258e3de731bdf0f7
root <thomas.rousseau19@gmail.com>
2025-02-25 10:14:23 +0000

    r

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
