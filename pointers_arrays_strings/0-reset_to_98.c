ba40342653b24cdf818b8a3dc0b5d801f5d06a4d
root <thomas.rousseau19@gmail.com>
2025-02-25 09:40:24 +0000

    *n=

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - Entry point
+ *@n : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
