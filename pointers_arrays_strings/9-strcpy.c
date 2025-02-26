177cbddefcbc731325c9d5240a93fb4220af7e91
root <thomas.rousseau19@gmail.com>
2025-02-26 12:15:22 +0000

    r

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strcpy - Entry point
+ *@src : caractere a verifier
+ *@dest : azrr
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+int len;
+
+len = 0;
+while (src[len] != '\0')
+len++;
+
+for (i = 0 ; i < len ; i++)
+{
+dest[i] = src[i];
+}
+return (src);
+}
+
+
