ad09ddb433dc50f6f8e2114ddf44bd1b6133ecd8
root <thomas.rousseau19@gmail.com>
2025-02-26 13:05:33 +0000

    r

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+
+int _atoi(char *s)
+{
+int res;
+
+res = atoi(s);
+return(res);
+}
