9bca40c4c4facd27c70200e64b7195a485763e69
root <thomas.rousseau19@gmail.com>
2025-02-26 19:59:49 +0000

    r

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,6 +9,25 @@
  * Return: Always 0 (Success)
  */
 
+void rev_string(char *s)
+{
+int i = 0, len;
+len = _strlen(s) - 1;
+while (len > i)
+	{
+	swap_char(s + len, s + i);
+	i++;
+	len--;
+	}
+}
+
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
 int _strlen(char *s)
 {
 int len;
@@ -20,30 +39,23 @@ len++;
 return (len);
 }
 
-void rev_string(char *s)
-{
-/*
-int len;
-int i;
+/**
+ *swap_char - Entry point
+ *@a : caractere a verifier
+ *@b : swapper
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
 
-len = _strlen(s);
-char temp[len];
-for (i = 0 ; i < len + 1  ; i++)
+
+void swap_char(char *a, char *b)
 {
-temp[i] = s[len - (i)];
+char swap = *a;
+
+*a = *b;
+*b = swap;
+
 }
-for (i = 0 ; i < len + 1  ; i++)
-{
-s[i] = temp[len - (i)];
-}*/
 
-char temp;
-    int n = _strlen(s)-1,i;
 
- for(i = 0 ; i < n / 2 ; i++)
-{
-    temp = s[i];
-    s[i] = s[n-i];
-    s[n-i] = temp;
- }
-}
