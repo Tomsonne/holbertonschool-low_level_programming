ecf5b923c5667fbe88b19b2fc3fce2e50b13e039
root <thomas.rousseau19@gmail.com>
2025-02-27 12:42:30 +0000

    t

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_strncat - Entry point
+ *@dest : caractere a verifier
+ *@src :azeze
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int lenD = _strlen(dest);
+int lenS = _strlen(src);
+
+
+for (i = 0 ; i < lenS ; i++)
+{
+if (i < n)
+dest[lenD + i] = src[i];
+}
+
+return (dest);
+
+}
+
+
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+return (len);
+}
