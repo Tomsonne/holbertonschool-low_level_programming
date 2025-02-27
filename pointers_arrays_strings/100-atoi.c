64ff0b28332bbdf8e49272fad4ef750294550d71
root <thomas.rousseau19@gmail.com>
2025-02-27 08:23:28 +0000

    t

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- *_strlen - Entry point
+ *_atoi - Entry point
  *@s : caractere a verifier
  *int *n: caractere a verifier
  *fonction: ecris alphabet
@@ -13,29 +13,16 @@
 
 int _atoi(char *s)
 {
-int i;
-int res;
-int val;
+int i = 1;
+unsigned int num = 0;
 
-i = 0;
-val = 0;
-if ((s[i] == 45 || s[i]) >= (48 && s[i] <= 57))
-	{
-	while ((s[i] == 45 || s[i]) >= (48 && s[i] <= 57))
-		{
-		if (s[i] == 45)
-			res = res * -1;
-		else if (s[i] >= 48 && s[i] <= 57)
-			{
-				res[val] = s[i];
-				val++;
-			}
-		i = i + 1;
-		}
-	}
-else
-	_putchar('0');
-
-return (res);
-
+do {
+if (*s == '-')
+i *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *i);
 }
