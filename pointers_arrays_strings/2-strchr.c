b40c75b37fd6589c51cc17e152be5dc69fbdbdbe
root <thomas.rousseau19@gmail.com>
2025-03-03 10:13:03 +0000

    t

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,38 @@
 char *_strchr(char *s, char c)
 {
 int i;
+int len = _strlen(s);
 
-while(s[i] != c)
+for (i = 0; i  < len ; i++)
 {
-i++;
+if(s[i] == c)
+return(s + i);
 }
 
-return(s + i);
+
+
+return(NULL);
+}
+
+
+
+
+
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+return (len);
 }
