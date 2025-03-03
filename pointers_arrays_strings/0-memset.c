08a64a8806057d99d47e48af64f23a43b119533b
root <thomas.rousseau19@gmail.com>
2025-03-03 08:58:36 +0000

    r

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memset - Entry point
+ *@b : caractere a verifier
+ *@s :azeze
+ *@n : number
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  char *m = *s;
+  unsigned int  i = 0;
+
+while (i < n)
+    {
+      m[i] = b;
+      i++;
+    }
+  return (s);
+}
