da66ff2678abffaa0cc66fa367d183808628aa8c
root <thomas.rousseau19@gmail.com>
2025-03-03 11:02:38 +0000

    t

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_memcpy - Entry point
+ *@dest: caractere a verifier
+ *@src:azeze
+ *@n: number
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int  res = 0;
+int lenS = _strlen(s);
+int lenA = _strlen(accept);
+int b1, b2;
+
+
+for (b2 = 0 ; b2 < lenA ; b2++)
+{
+	for (b1 = 0 ; b1 < lenS ; b1++)
+        {
+	if (s[b1] == accept[b2])
+		{
+		res++;
+		b1 = lenS;
+		}
+	}
+}
+return(res+1);
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+return (len);
+}
