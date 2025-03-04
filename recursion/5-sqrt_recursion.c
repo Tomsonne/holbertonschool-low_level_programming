f197ea8183b7bc0a815501571d1106231136583c
root <thomas.rousseau19@gmail.com>
2025-03-04 13:23:22 +0000

    t

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
- *_sqrt_recursion - Entry point
- *@s : caractere a verifier
+ *_sqrt_recursion_help - Entry point
+ *@mult: erer
+ *@n : caractere a verifier
  *int *n: caractere a verifier
  *fonction: ecris alphabet
  * Return: Always 0 (Success)
@@ -14,17 +15,25 @@ int _sqrt_recursion_help(int n, int mult)
 {
 
 if (mult * mult == n)
-return mult;
+return (mult);
 if (mult * mult > n)
-return -1;
+return (-1);
 
 return (_sqrt_recursion_help(n, mult + 1));
 }
 
+
+/**
+ *_sqrt_recursion - Entry point
+ *@n : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return -1;
+return (-1);
 
 return (_sqrt_recursion_help(n, 0));
 }
