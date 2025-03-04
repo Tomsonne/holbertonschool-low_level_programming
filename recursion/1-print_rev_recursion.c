a0429bdfff780c95a24e9c6c3941188cdbb9b110
root <thomas.rousseau19@gmail.com>
2025-03-04 10:02:50 +0000

    t

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_print_rev_recursion - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
+}
