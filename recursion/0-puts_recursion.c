b10ef9e3e8e9dd9f8f2021775769e76c716ccfe9
root <thomas.rousseau19@gmail.com>
2025-03-04 09:06:34 +0000

    t

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -22,21 +22,4 @@ _puts_recursion(s + 1);
 
 
 }
-/**
- *_strlen - Entry point
- *@s : caractere a verifier
- *int *n: caractere a verifier
- *fonction: ecris alphabet
- * Return: Always 0 (Success)
- */
 
-int _strlen(char *s)
-{
-int len;
-
-len = 0;
-while (s[len] != '\0')
-len++;
-
-return (len);
-}
