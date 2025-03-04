1481170a47bccaf56de808f4725834430f5a331b
root <thomas.rousseau19@gmail.com>
2025-03-04 10:50:22 +0000

    t

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+ /**
+ *factorial - Entry point
+ *@n : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+
+if (n < 0)
+return (-1);
+
+if (n == 0)
+return (1);
+
+return (n * factorial(n - 1));
+
+}
