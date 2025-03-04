b74f388a18433adb08a5005bbd6526decd9fdf4e
root <thomas.rousseau19@gmail.com>
2025-03-04 09:05:04 +0000

    t

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts_recursion - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_putchar(*s);
+_puts_recursion(s + 1);
+
+
+}
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+return (len);
+}
