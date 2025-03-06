1fe4843d0dcbf68704e275624a0d02a1c31851d9
root <thomas.rousseau19@gmail.com>
2025-03-06 09:11:17 +0000

    t

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,39 +2,7 @@
 #include "main.h"
 
 /**
- *is_prime_number - Entry point
- *@s : caractere a verifier
- *int *n: caractere a verifier
- *fonction: ecris alphabet
- * Return: Always 0 (Success)
- */
-
-
-int is_prime_number(int n)
-{
-int sqrt;
-int i = 3;
-
-sqrt = _sqrt_recursion(n);
-if (n <= 1)
-return (0);
-if ((n % 2) == 0)
-return (1);
-
-for (i = 3; i < sqrt; i++)
-{
-if ((sqrt % i) == 0)
-return (0);
-}
-return (1);
-
-}
-
-
-
-
-/**
- *_sqrt_recursion_help - Entry point
+ *is_prime_number_help - Entry point
  *@mult: erer
  *@n : caractere a verifier
  *int *n: caractere a verifier
@@ -43,29 +11,35 @@ return (1);
  */
 
 
-int _sqrt_recursion_help(int n, int mult)
+int is_prime_number_help(int n, int mult)
 {
 
-if (mult * mult == n)
-return (mult);
 if (mult * mult > n)
-return (-1);
+return (1);
+if (n % mult == 0)
+return (0);
 
-return (_sqrt_recursion_help(n, mult + 1));
+return (is_prime_number_help(n, mult + 2));
 }
 
 
 /**
- *_sqrt_recursion - Entry point
+ *is_prime_number - Entry point
  *@n : caractere a verifier
  *int *n: caractere a verifier
  *fonction: ecris alphabet
  * Return: Always 0 (Success)
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-if (n < 0)
-return (-1);
+if (n <= 1)
+return (0);
 
-return (_sqrt_recursion_help(n, 0));
+if (n == 2)
+return (1);
+
+if ((n % 2) == 0 && n > 2)
+return (0);
+
+return (is_prime_number_help(n, 3));
 }
