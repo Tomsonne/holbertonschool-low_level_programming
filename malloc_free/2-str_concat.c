bfaeca06a61a5e5202a8b84d3031c3e52b8543aa
root <thomas.rousseau19@gmail.com>
2025-03-11 10:19:53 +0000

    t

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,1 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - Entry point
+ *@s1 : caractere a verifier
+ *@s2 : azr
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+char *res;
+int index, ttindex = 0, lenght = 0;
+
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (index = 0; s1[index] || s2[index]; index++)
+lenght++;
+
+res = malloc(sizeof(char) * lenght);
+
+if (res == NULL)
+return (NULL);
+
+for (index = 0; s1[index]; index++)
+res[ttindex++] = s1[index];
+
+for (index = 0; s2[index]; index++)
+res[ttindex++] = s2[index];
+
+return (res);
+
+}
