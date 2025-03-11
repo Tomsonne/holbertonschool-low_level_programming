6fd2c177f6ee3837c69fa89328ed51aeb368c71b
root <thomas.rousseau19@gmail.com>
2025-03-11 09:53:21 +0000

    t

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ *create_array - Entry point
+ *@size : caractere a verifier
+ *@c : azazr
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
 char *create_array(unsigned int size, char c)
 {
 
@@ -14,10 +23,10 @@ return (NULL);
 array = malloc(size * sizeof(char));
 
 if (array == NULL)
-        return NULL;
+return (NULL);
 
 for (i = 0; i < size; i++)
-        array[i] = c;
+array[i] = c;
 
 return (array);
 
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - Entry point
+ *@str : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+unsigned int lenght;
+char *res;
+unsigned int i;
+
+if (str == NULL)
+return (NULL);
+
+lenght = 0;
+
+while (str[lenght] != '0')
+lenght++;
+
+res = (char *)malloc((lenght + 1) * sizeof(char));
+
+for (i = 0; i < lenght; i++)
+res[i] = str[i];
+
+res[lenght] = '\0';
+
+return (res);
+}
