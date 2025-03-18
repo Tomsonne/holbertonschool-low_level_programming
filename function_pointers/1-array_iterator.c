1a49cd5a850c6b091cbb5d46302eb497c9106d1a
root <thomas.rousseau19@gmail.com>
2025-03-18 13:08:57 +0000

    t

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,3 +1,18 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - a
+ * @size: string
+ * @action: pointeur de fonction
+ *@array: array
+ */
 
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t index = 0;
 
-void print_name(char *name, void (*f)(char *));
+for (index = 0; index < size; index++)
+action(array[index]);
+
+}
+
