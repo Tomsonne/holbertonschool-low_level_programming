7d72c0f038153bbdc2434c1ad01b2b9839d9ed83
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-03 13:49:19 +0200

    5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - add
+ * @head: double pointer
+ * @index: int value
+ * Return: Address
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i;
+
+i = 0;
+while (i < index)
+{
+if (head->next == NULL)
+return (NULL);
+head = head->next;
+i++;
+}
+return (head);
+}
