afe6d62122e121099cc9f46596574d3415e98312
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-03 10:51:35 +0200

    r

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,26 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
+
+/**
+ *_strlen - Entry point
+ *@s : caractere a verifier
+ *int *n: caractere a verifier
+ *fonction: ecris alphabet
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+return (len);
+}
 
 /**
  * add_node_end - ajoute
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -24,13 +24,3 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-int _strlen(char *s)
-{
-int len;
-
-len = 0;
-while (s[len] != '\0')
-len++;
-
-return (len);
-}
