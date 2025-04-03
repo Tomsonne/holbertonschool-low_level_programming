7a06f9e6c2dc1c55c8ced6a328db60f22c1d1cda
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-03 13:50:53 +0200

    5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,13 +11,21 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 unsigned int i;
 
-i = 0;
-while (i < index)
-{
-if (head->next == NULL)
+if (head == NULL)
 return (NULL);
+
+while (head->prev != NULL)
+head = head->prev;
+
+i = 0;
+
+while (head != NULL)
+{
+if (i == index)
+break;
 head = head->next;
 i++;
 }
+
 return (head);
 }
