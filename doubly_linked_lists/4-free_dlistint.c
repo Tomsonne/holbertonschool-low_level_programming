25c64c1e1e301f99a2726ffed3815efc93931ff6
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-03 13:31:36 +0200

    r

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,9 +9,10 @@ void free_dlistint(dlistint_t *head)
 dlistint_t *temp;
 
 if (head != NULL)
-    while (head->prev != NULL)
-        head = head->prev;
-
+{
+while (head->prev != NULL)
+head = head->prev;
+}
 
 while (head != NULL)
 {
