51db83be3cd817b959172aab1a029c37058a50c1
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-04 11:35:57 +0200

    r

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,44 +10,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+dlistint_t *new;
+dlistint_t *head;
+unsigned int i;
 
-	new = NULL;
-	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
-	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
-	}
+new = NULL;
+if (idx == 0)
+new = add_dnodeint(h, n);
+else
+{
+head = *h;
+i = 1;
 
-	return (new);
+while (head != NULL && i < idx)
+{
+head = head->next;
+i++;
 }
+
+if (head == NULL)
+
+new = malloc(sizeof(dlistint_t));
+if (new != NULL)
+{
+new->n = n;
+new->next = head->next;
+new->prev = head;
+
+if (head->next != NULL)
+head->next->prev = new;
+head->next = new;
+}
+}
+
+return (new);
+}
+
