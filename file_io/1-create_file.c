b86419ef90529fa2c1c09b527d28fbfcf0c1f88e
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-04 15:14:56 +0200

    r

---
+++ file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * create_file - creates
+ * @filename: name
+ * @text_content: text
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int len = 0, indlen = 0;
+	char *ptr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (indlen = 0, ptr = text_content; *ptr; ptr++)
+			indlen++;
+		len = write(file, text_content, indlen);
+	}
+
+	if (close(file) == -1 || indlen != len)
+		return (-1);
+	return (1);
+}
+
